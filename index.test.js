2ec8707d0dcccdf335c1d9da11080ad17269e505
Tiaan <tiaanduplessis@hotmail.com>
2017-08-02 17:52:13 +0200

    Initial test setup

--- index.test.js
+++ index.test.js
@@ -1,5 +1,15 @@
-const Luno = require('./')
+const Luno = require('./index')
+
+const options = {
+  key: process.env.LUNO_KEY,
+  secret: process.env.LUNO_SECRET
+}
 
 test('should be defined', () => {
   expect(Luno).toBeDefined()
 })
+
+// test('should return new Luno client instance', () => {
+//   const client = new Luno(options)
+//   expect(client.getTicker).toBeDefined()
+// })
